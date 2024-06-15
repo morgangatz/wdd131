6b9002bc5783c02d30608290878bc533e2e2c078
morgangatz <gatzmorgan@gmail.com>
2024-06-15 10:31:57 -0500

    finished styling, get date modified to work

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,15 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Get the current year
     const currentYear = new Date().getFullYear();
+    // Output the current year in the footer's first paragraph
+    document.getElementById('currentYear').textContent = `© ${currentYear} ❄ Morgan Gatz ❄ Canada`;
 
-    // Get the last modified date
+    // Get the last modified date of the document
     const lastModified = document.lastModified;
-
-    // Set the content of the copyright paragraph
-    const copyrightElement = document.getElementById("copyright");
-    copyrightElement.textContent = `© ${currentYear}`;
-
-    // Set the content of the last modified paragraph
-    const lastModifiedElement = document.getElementById("lastModified");
-    lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+    // Output the last modified date in the footer's second paragraph
+    document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
 });
